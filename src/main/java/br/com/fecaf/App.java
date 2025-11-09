0ae6ba6313e41012f24c7312a75e369d9dbc510b
LucasSena <destroyergamer2021@gmail.com>
2025-11-09 14:55:28 -0300

    Correção dos erros das dependencias no aquivo pom.xml

--- src/main/java/br/com/fecaf/App.java
+++ src/main/java/br/com/fecaf/App.java
@@ -2,8 +2,10 @@ package br.com.fecaf;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
